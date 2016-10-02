4899b437b963c420a22161b71a106b8f9e1bed3b
woxtu <woxtup@gmail.com>
2016-10-03 02:57:39 +0900

    Hello, Curl!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate curl_sys;
+
+use std::ffi::CString;
+
 fn main() {
-    println!("Hello, world!");
+  unsafe {
+    let handle = curl_sys::curl_easy_init();
+    let url = CString::new("http://example.com").unwrap();
+    curl_sys::curl_easy_setopt(handle, curl_sys::CURLOPT_URL, url.as_ptr());
+    curl_sys::curl_easy_perform(handle);
+    curl_sys::curl_easy_cleanup(handle);
+  }
 }
