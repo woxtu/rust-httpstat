5b4feb6600d7c9ab03b901783418114a86149244
woxtu <woxtup@gmail.com>
2016-10-14 23:13:25 +0900

    Create curl module

---
+++ src/curl.rs
@@ -0,0 +1,119 @@
+use std::{error, mem, slice, str};
+use std::cell::RefCell;
+use std::ffi::CString;
+use std::rc::{Rc, Weak};
+use curl_sys::*;
+use libc::*;
+
+type Error = Box<error::Error>;
+
+pub struct Easy {
+  handle: *mut CURL,
+  header_buffer: Rc<RefCell<String>>,
+  body_buffer: Rc<RefCell<String>>,
+  error_buffer: Vec<u8>,
+}
+
+pub struct Response {
+  pub header: String,
+  pub body: String,
+}
+
+#[derive(Debug)]
+pub struct Time {
+  pub namelookup: f64,
+  pub connect: f64,
+  pub pretransfer: f64,
+  pub starttransfer: f64,
+  pub total: f64,
+}
+
+impl Easy {
+  pub fn new() -> Result<Easy, Error> {
+    unsafe {
+      match curl_easy_init().as_mut() {
+        Some(handle) => {
+          let header_buffer = Rc::new(RefCell::new(String::new()));
+          curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, write_function as *mut c_void);
+          curl_easy_setopt(handle, CURLOPT_HEADERDATA, Rc::downgrade(&header_buffer));
+
+          let body_buffer = Rc::new(RefCell::new(String::new()));
+          curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_function as *mut c_void);
+          curl_easy_setopt(handle, CURLOPT_WRITEDATA, Rc::downgrade(&body_buffer));
+
+          let error_buffer = vec!(0; CURL_ERROR_SIZE);
+          curl_easy_setopt(handle, CURLOPT_ERRORBUFFER, error_buffer.as_ptr());
+
+          Ok(Easy {
+            handle: handle,
+            header_buffer: header_buffer,
+            body_buffer: body_buffer,
+            error_buffer: error_buffer,
+          })
+        },
+        None => Err(From::from("Failed to create an easy handle")),
+      }
+    }
+  }
+
+  pub fn set_url(&self, url: &str) -> Result<(), Error> {
+    let url = try!(CString::new(url));
+    self.set_option(CURLOPT_URL, url.as_ptr())
+  }
+
+  pub fn perform(&self) -> Result<Response, Error> {
+    self.get_result(unsafe { curl_easy_perform(self.handle) })
+      .map(|_| Response {
+        header: self.header_buffer.borrow().trim().to_string(),
+        body: self.body_buffer.borrow().trim().to_string(),
+      })
+  }
+
+  pub fn get_time(&self) -> Result<Time, Error> {
+    Ok(Time {
+      namelookup: try!(self.get_info(CURLINFO_NAMELOOKUP_TIME)),
+      connect: try!(self.get_info(CURLINFO_CONNECT_TIME)),
+      pretransfer: try!(self.get_info(CURLINFO_PRETRANSFER_TIME)),
+      starttransfer: try!(self.get_info(CURLINFO_STARTTRANSFER_TIME)),
+      total: try!(self.get_info(CURLINFO_TOTAL_TIME)),
+    })
+  }
+
+  fn get_result(&self, code: CURLcode) -> Result<(), Error> {
+    match code {
+      CURLE_OK => Ok(()),
+      _ => Err(From::from(
+        str::from_utf8(&self.error_buffer).map(|s| s.trim_matches('\u{0}')).unwrap_or("Unknown error"))),
+    }
+  }
+
+  fn set_option<T>(&self, option: CURLoption, value: T) -> Result<(), Error> {
+    self.get_result(unsafe { curl_easy_setopt(self.handle, option, value) })
+  }
+
+  fn get_info<T: Default>(&self, info: CURLINFO) -> Result<T, Error> {
+    let mut result = T::default();
+    self.get_result(unsafe { curl_easy_getinfo(self.handle, info, &mut result) })
+      .map(|_| result)
+  }
+}
+
+impl Drop for Easy {
+  fn drop(&mut self) {
+    unsafe {
+      curl_easy_cleanup(self.handle)
+    }
+  }
+}
+
+unsafe extern fn write_function(ptr: *mut c_char, size: size_t, nitems: size_t, userdata: *mut c_void) -> size_t {
+  let buffer = mem::transmute::<_, Weak<RefCell<String>>>(userdata);
+
+  if let Some(buffer) = buffer.upgrade() {
+    if let Ok(line) = str::from_utf8(slice::from_raw_parts(ptr as *mut _, size * nitems)) {
+      buffer.borrow_mut().push_str(line);
+    }
+  }
+
+  size * nitems
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,21 @@
 extern crate curl_sys;
 extern crate libc;
 
-use std::ffi::CString;
-use std::slice;
-use libc::{c_char, size_t};
+mod curl;
 
-unsafe extern fn write_function(ptr: *mut c_char, size: size_t, nmemb: size_t, buffer: &mut String) -> size_t {
-  buffer.push_str(&String::from_utf8_lossy(slice::from_raw_parts(ptr as *mut _, size * nmemb)));
-
-  size * nmemb
+macro_rules! unwrap {
+  ($expr:expr) => (match $expr {
+    Ok(value) => value,
+    Err(error) => panic!(error.to_string()),
+  })
 }
 
 fn main() {
-  unsafe {
-    let handle = curl_sys::curl_easy_init();
-    let url = CString::new("http://example.com").unwrap();
-    curl_sys::curl_easy_setopt(handle, curl_sys::CURLOPT_URL, url.as_ptr());
+  let client = unwrap!(curl::Easy::new());
+  unwrap!(client.set_url("http://example.com"));
 
-    let mut header = String::new();
-    curl_sys::curl_easy_setopt(handle, curl_sys::CURLOPT_HEADERFUNCTION, write_function as *const c_char);
-    curl_sys::curl_easy_setopt(handle, curl_sys::CURLOPT_HEADERDATA, &mut header);
-
-    let mut body = String::new();
-    curl_sys::curl_easy_setopt(handle, curl_sys::CURLOPT_WRITEFUNCTION, write_function as *const c_char);
-    curl_sys::curl_easy_setopt(handle, curl_sys::CURLOPT_WRITEDATA, &mut body);
-
-    curl_sys::curl_easy_perform(handle);
-
-    let mut time_namelookup = 0f64;
-    curl_sys::curl_easy_getinfo(handle, curl_sys::CURLINFO_NAMELOOKUP_TIME, &mut time_namelookup);
-
-    let mut time_connect = 0f64;
-    curl_sys::curl_easy_getinfo(handle, curl_sys::CURLINFO_CONNECT_TIME, &mut time_connect);
-
-    let mut time_pretransfer = 0f64;
-    curl_sys::curl_easy_getinfo(handle, curl_sys::CURLINFO_PRETRANSFER_TIME, &mut time_pretransfer);
-
-    let mut time_starttransfer = 0f64;
-    curl_sys::curl_easy_getinfo(handle, curl_sys::CURLINFO_STARTTRANSFER_TIME, &mut time_starttransfer);
-
-    let mut time_total = 0f64;
-    curl_sys::curl_easy_getinfo(handle, curl_sys::CURLINFO_TOTAL_TIME, &mut time_total);
-
-    println!("{:7.0}", time_namelookup * 1000.0);
-    println!("{:7.0}", time_connect * 1000.0);
-    println!("{:7.0}", time_pretransfer * 1000.0);
-    println!("{:7.0}", time_starttransfer * 1000.0);
-    println!("{:7.0}", time_total * 1000.0);
-
-    curl_sys::curl_easy_cleanup(handle);
-  }
+  let response = unwrap!(client.perform());
+  println!("{}", response.header);
+  println!("{}", response.body);
+  println!("{:?}", client.get_time());
 }
