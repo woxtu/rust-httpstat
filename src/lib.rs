9aa064b7c7df421e1da4d6fd84da647414481941
Hideo Hattori <hattori-h@klab.com>
2018-03-05 11:48:10 +0900

    add wrapper function for easy use

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,33 @@ extern crate rand;
 
 pub mod httpstat;
 pub mod curl;
+
+pub fn request(url: &str) -> Result<(curl::Response, curl::Time), String> {
+    let client = curl::easy::Easy::new();
+    match client {
+        Err(e) => return Err(format!("{}", e)),
+        _ => {},
+    }
+    let client = client.unwrap();
+
+    match client.set_url(url) {
+        Err(e) => return Err(format!("{}", e)),
+        _ => {},
+    }
+
+    let response = client.perform();
+    match response {
+        Err(e) => return Err(format!("{}", e)),
+        _ => {},
+    }
+    let response = response.unwrap();
+
+    let time = client.get_time();
+    match time {
+        Err(e) => return Err(format!("{}", e)),
+        _ => {}
+    }
+    let time = time.unwrap();
+
+    Ok((response, time))
+}
